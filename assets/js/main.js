1762b9b93147c99f4cae0f9f401feeb65c662d4d
Mvcampbell3 <mvcampbell3@gmail.com>
2019-01-17 20:55:31 -0500

    added firebase cdn

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCyTlLrY30Yw_3eqKWmj4ejqmAAYL_X1-Y",
+    authDomain: "flu-fighters.firebaseapp.com",
+    databaseURL: "https://flu-fighters.firebaseio.com",
+    projectId: "flu-fighters",
+    storageBucket: "flu-fighters.appspot.com",
+    messagingSenderId: "356867364502"
+  };
+  firebase.initializeApp(config);
